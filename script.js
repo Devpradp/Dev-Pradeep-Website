ef30c44bf10f660fa2bc9e873cf914bc3171ce73
Devpradp <107737926+Devpradp@users.noreply.github.com>
2022-09-05 17:57:10 -0400

    linked buttons

--- script.js
+++ script.js
@@ -5,3 +5,4 @@ document.querySelector('.header__moon').onclick = function() {
 document.querySelector('.header__mob-menu').onclick = function() {
     document.querySelector('.header-menu').classList.toggle('is-active');
 }
+
